1c6e7f49c60b03c1fd8269fa4166e4112597524c
Chukwunonso Frank <franknonso114@gmail.com>
2025-09-02 12:00:59 +0100

    wip

--- resources/views/livewire/dashboard/robot.blade.php
+++ resources/views/livewire/dashboard/robot.blade.php
@@ -101,10 +101,28 @@
                     </div>
 
                     <div class="flex-1">
-                        <label for="input-label" class="block text-sm font-medium mb-2 text-zinc-300">Markets</label>
+                        <label for="input-label" class="block text-sm font-medium mb-2 text-zinc-300">Choose
+                            Account</label>
+                        <div class="grid grid-cols-2 gap-x-2">
+                            <label for="hs-vertical-radio-in-form-demo"
+                                wire:click="selectAccountType('Demo account', 'demo')"
+                                class="px-4 py-2 w-full {{ $this->accountTypeSlug === 'demo' ? 'border-2 border-accent' : 'border border-[#26252a]' }} bg-transparent rounded-lg text-base focus:border-blue-500 focus:ring-blue-500">
+                                <div class="flex-1 text-center text-white">
+                                    <h2>Demo</h2>
+                                </div>
+                            </label>
+                            <label for="hs-vertical-radio-in-form-live"
+                                wire:click="selectAccountType('Live account', 'live')"
+                                class="px-4 py-2 w-full {{ $this->accountTypeSlug === 'live' ? 'border-2 border-accent' : 'border border-[#26252a]' }} bg-transparent rounded-lg text-base focus:border-blue-500 focus:ring-blue-500">
+                                <div class="flex-1 text-center text-white">
+                                    <h2>Live</h2>
+                                </div>
+                            </label>
+                        </div>
+                        {{-- <label for="input-label" class="block text-sm font-medium mb-2 text-zinc-300">Markets</label>
                         <input type="text" value="Forex & Crypto"
                             class="border border-[#26252a] bg-transparent text-white text-start text-sm py-2.5 sm:py-3 px-4 block w-full rounded-lg sm:text-sm focus:outline-0"
-                            placeholder="" readonly>
+                            placeholder="" readonly> --}}
                     </div>
                 </div>
 
@@ -183,8 +201,7 @@
                         <div
                             class="flex items-center justify-center gap-x-1 w-full text-sm self-center text-center border border-[#26252a] bg-transparent py-2.5 sm:py-3 px-4 rounded-lg text-[#FFFFFF] focus:outline-0">
                             <div class="flex-none">
-                                <img class="inline" src="{{ asset('assets/icons/binance.svg') }}"
-                                    alt="binance-logo">
+                                <img class="inline" src="{{ asset('assets/icons/binance.svg') }}" alt="binance-logo">
                             </div>
                             <div class="flex-none w-4">
                                 <svg width="16" height="16" viewBox="0 0 16 16" fill="none"
@@ -305,47 +322,6 @@
                                 </label>
                             </div>
                         @endforeach
-                    </div>
-                </div>
-
-                <div class="mb-4">
-                    <label for="input-label" class="block text-sm font-medium mb-2 text-zinc-300">Choose
-                        Account</label>
-                    <div class="grid grid-cols-2 gap-x-2">
-                        <label for="hs-vertical-radio-in-form-demo"
-                            wire:click="selectAccountType('Demo account', 'demo')"
-                            class="flex p-4 gap-x-2 items-center w-full bg-transparent border border-[#26252a] rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500">
-                            <div class="flex-1 text-center text-white">
-                                <h2>Demo Account</h2>
-                                <h2 class="font-bold">@money(auth()->user()->demo_balance / 100)</h2>
-                            </div>
-                            <div class="flex-none w-4">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
-                                    viewBox="0 0 24 24" fill="#3b71ff"
-                                    class="{{ $this->accountTypeSlug === 'demo' ? 'inline' : 'hidden' }} icon icon-tabler icons-tabler-filled icon-tabler-circle-check">
-                                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                                    <path
-                                        d="M17 3.34a10 10 0 1 1 -14.995 8.984l-.005 -.324l.005 -.324a10 10 0 0 1 14.995 -8.336zm-1.293 5.953a1 1 0 0 0 -1.32 -.083l-.094 .083l-3.293 3.292l-1.293 -1.292l-.094 -.083a1 1 0 0 0 -1.403 1.403l.083 .094l2 2l.094 .083a1 1 0 0 0 1.226 0l.094 -.083l4 -4l.083 -.094a1 1 0 0 0 -.083 -1.32z" />
-                                </svg>
-                            </div>
-                        </label>
-                        <label for="hs-vertical-radio-in-form-live"
-                            wire:click="selectAccountType('Live account', 'live')"
-                            class="flex p-4 gap-x-2 items-center w-full border border-[#26252a] bg-transparent rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500">
-                            <div class="flex-1 text-center text-white">
-                                <h2>Live Account</h2>
-                                <h2 class="font-bold">@money(auth()->user()->live_balance / 100)</h2>
-                            </div>
-                            <div class="flex-none w-4">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
-                                    viewBox="0 0 24 24" fill="#3b71ff"
-                                    class="{{ $this->accountTypeSlug === 'live' ? 'inline' : 'hidden' }} icon icon-tabler icons-tabler-filled icon-tabler-circle-check">
-                                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                                    <path
-                                        d="M17 3.34a10 10 0 1 1 -14.995 8.984l-.005 -.324l.005 -.324a10 10 0 0 1 14.995 -8.336zm-1.293 5.953a1 1 0 0 0 -1.32 -.083l-.094 .083l-3.293 3.292l-1.293 -1.292l-.094 -.083a1 1 0 0 0 -1.403 1.403l.083 .094l2 2l.094 .083a1 1 0 0 0 1.226 0l.094 -.083l4 -4l.083 -.094a1 1 0 0 0 -.083 -1.32z" />
-                                </svg>
-                            </div>
-                        </label>
                     </div>
                 </div>
 
