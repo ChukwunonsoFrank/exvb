4e3e394b3bc664ef7330e82dd7758a11123cf237
Chukwunonso Frank <franknonso114@gmail.com>
2025-10-09 23:41:01 +0100

    wip

--- resources/views/vendor/mail/html/message.blade.php
+++ resources/views/vendor/mail/html/message.blade.php
@@ -1,29 +1,29 @@
 <x-mail::layout>
-    {{-- Header --}}
-    <x-slot:header>
-        <x-mail::header :url="config('app.url')">
-            {{ config('app.name') }}
-        </x-mail::header>
-    </x-slot:header>
+{{-- Header --}}
+<x-slot:header>
+<x-mail::header :url="config('app.url')">
+    {{ config('app.name') }}
+</x-mail::header>
+</x-slot:header>
 
-    {{-- Body --}}
-    {{ $slot }}
+{{-- Body --}}
+{{ $slot }}
 
-    {{-- Subcopy --}}
-    @isset($subcopy)
-        <x-slot:subcopy>
-            <x-mail::subcopy>
-                {{ $subcopy }}
-            </x-mail::subcopy>
-        </x-slot:subcopy>
-    @endisset
+{{-- Subcopy --}}
+@isset($subcopy)
+<x-slot:subcopy>
+<x-mail::subcopy>
+    {{ $subcopy }}
+</x-mail::subcopy>
+</x-slot:subcopy>
+@endisset
 
-    {{-- Footer --}}
-    <x-slot:footer>
-        <x-mail::footer>
-            {{ __('moxyai is a trademark of Moxyai Inc with the registered address at 2 Av. Jean Jaurès, 92120 Châtillon, France.') }}
-            <br><br>
-            © {{ date('Y') }} {{ config('app.name') }} Inc. {{ __('All rights reserved.') }}
-        </x-mail::footer>
-    </x-slot:footer>
+{{-- Footer --}}
+<x-slot:footer>
+<x-mail::footer>
+{{ __('moxyai is a trademark of Moxyai Inc with the registered address at 2 Av. Jean Jaurès, 92120 Châtillon, France.') }}
+<br><br>
+© {{ date('Y') }} {{ config('app.name') }} Inc. {{ __('All rights reserved.') }}
+</x-mail::footer>
+</x-slot:footer>
 </x-mail::layout>
