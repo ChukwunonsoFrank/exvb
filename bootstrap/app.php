6fb5b065617d5c77f8ba1faca9611af32587eb40
Chukwunonso Frank <franknonso114@gmail.com>
2025-10-09 08:50:06 +0100

    wip

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,20 +3,33 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Request;
 
 return Application::configure(basePath: dirname(__DIR__))
-    ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
-        channels: __DIR__.'/../routes/channels.php',
-        health: '/up',
-    )
-    ->withMiddleware(function (Middleware $middleware) {
-        $middleware->alias([
-            'admin' => \App\Http\Middleware\Admin::class,
-            'user' => \App\Http\Middleware\User::class,
-        ]);
-    })
-    ->withExceptions(function (Exceptions $exceptions) {
-        //
-    })->create();
+  ->withRouting(
+    web: __DIR__ . '/../routes/web.php',
+    commands: __DIR__ . '/../routes/console.php',
+    channels: __DIR__ . '/../routes/channels.php',
+    health: '/up',
+  )
+  ->withMiddleware(function (Middleware $middleware) {
+    $middleware->alias([
+      'admin' => \App\Http\Middleware\Admin::class,
+      'user' => \App\Http\Middleware\User::class,
+    ]);
+
+    $middleware->trustProxies(at: [
+      '10.0.1.6',
+    ]);
+
+    $middleware->trustProxies(
+      headers: Request::HEADER_X_FORWARDED_FOR |
+        Request::HEADER_X_FORWARDED_HOST |
+        Request::HEADER_X_FORWARDED_PORT |
+        Request::HEADER_X_FORWARDED_PROTO |
+        Request::HEADER_X_FORWARDED_AWS_ELB
+    );
+  })
+  ->withExceptions(function (Exceptions $exceptions) {
+    //
+  })->create();
