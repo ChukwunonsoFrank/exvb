7f52e299840bfc9a0b4c69dd58f2bb73564a909f
Chukwunonso Frank <franknonso114@gmail.com>
2025-10-20 21:00:41 +0100

    fix favicon

--- resources/views/components/layouts/landing-page.blade.php
+++ resources/views/components/layouts/landing-page.blade.php
@@ -22,6 +22,9 @@
     <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
     <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
     <link rel="manifest" href="/site.webmanifest">
+    <link rel="icon" href="/favicon.ico" type="image/x-icon">
+    <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
+    {{-- <link rel="icon" type="image/png" sizes="48x48" href="{{ asset('favicon-48x48.png') }}"> --}}
 
     <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
 
